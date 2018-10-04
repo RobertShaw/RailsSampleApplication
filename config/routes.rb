f07d80ec3783e58d25afae51b68ad5d0e32b1ffc
Robert Shaw <robleeshaw@gmail.com>
2018-10-04 10:58:58 -0700

    headers and footers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
-  get 'static_pages/home'
   
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help', as: 'help'
 
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
+
+  get '/contact', to: 'static_pages#contact'
 end
