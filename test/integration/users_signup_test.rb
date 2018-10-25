4a21c555282d5c124b7d51a18f12adc28ce17245
Robert Shaw <robleeshaw@gmail.com>
2018-10-24 23:07:09 -0700

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
   test "invalid signup information" do
     get signup_path
     assert_select 'form[action="/signup"]'
@@ -32,5 +33,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_not flash.empty?
     assert_select 'div.alert'
+    assert is_logged_in?
   end
+  
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
 end
