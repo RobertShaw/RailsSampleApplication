6952381ddfb0d967a4691b9fc81603b9d518ac29
Robert Shaw <robleeshaw@gmail.com>
2018-12-04 16:40:37 -0800

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   get '/help', to: 'static_pages#help', as: 'help'
 
@@ -22,4 +23,10 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -14,9 +14,12 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
+    assert_select "strong#following", text: @user.following.count.to_s
+    assert_select "strong#followers", text: @user.followers.count.to_s
     assert_select 'div.pagination', count: 1
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
   end
+
 end
